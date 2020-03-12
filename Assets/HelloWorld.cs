dcc93ce4a73d8048d1cdb8ed13cf6b5726294b6b
kevin_bernandino <kevinbernandino@gmail.com>
2020-03-12 00:24:43 -0700

    second commit

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -6,7 +6,7 @@ public class HelloWorld : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()   {
-        print("Hello World");
+        print("Hello Kevin B.");
     }
 
     // Update is called once per frame
